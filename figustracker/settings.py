502c7af729101f8285eece34276dc0434912ab1f
Herlein <julian.n.herlein@accenture.com>
2022-09-16 16:48:57 -0300

    dono

--- figustracker/settings.py
+++ figustracker/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^m7eztmcdybg_r!vt*%pklb(+o)fv1t00bamg&sts(vl@#^rl#"
+# SECRET_KEY = "django-insecure-^m7eztmcdybg_r!vt*%pklb(+o)fv1t00bamg&sts(vl@#^rl#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +122,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+import django_heroku
+django_heroku.settings(locals())
